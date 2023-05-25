481e2f5cc62e1ebdedccccd32a0dfe16f7cdb32c
Vinicius Freitas <91697213+viniciusolzon@users.noreply.github.com>
2023-05-24 23:00:47 -0300

    n instancia estoque table já que n usamos ela

--- tabelas/gerador.py
+++ tabelas/gerador.py
@@ -1,14 +1,12 @@
 from tabelas.cliente import ClienteTable
 from tabelas.livro import LivroTable
 from tabelas.pedido import PedidoTable
-from tabelas.estoque import EstoqueTable
 
 tables = {}
 
 tables['livro'] = LivroTable()
 tables['cliente'] = ClienteTable()
 tables['pedido'] = PedidoTable()
-tables['estoque'] = EstoqueTable()
 
 # Preenchendo a tabela de livros (rode o código uma vez com isso debaixo descomentado para preencher as tableas)
 # tables['livro'].insert(titulo = "Codigo Limpo", autor = "Robert Cecil Martin", ano_publicacao = 2008, preco = 69.99)
